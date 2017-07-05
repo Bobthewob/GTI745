75fefc1acdab281de3ac92f85a1650c786c21ab9
exp3rt1 <olivier.proulx.1@ens.etsmtl.ca>
2017-07-05 12:16:02 -0400

    la continuation de la marde

---
+++ Labo3/Assets/Scripts/MusicTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var transpose = -4;  // transpose in semitones
+        var note = -1; // invalid value to detect when note is pressed
+        var audio = GetComponent<AudioSource>();
+
+        if (Input.GetKeyDown("a"))
+        {
+            Debug.Log("a");
+            note = 0;  // C
+        }
+        if (Input.GetKeyDown("s")) note = 2;  // D
+        if (Input.GetKeyDown("d")) note = 4;  // E
+        if (Input.GetKeyDown("f")) note = 5;  // F
+        if (Input.GetKeyDown("g")) note = 7;  // G
+        if (Input.GetKeyDown("h")) note = 9;  // A
+        if (Input.GetKeyDown("j")) note = 11; // B
+        if (Input.GetKeyDown("k")) note = 12; // C
+        if (Input.GetKeyDown("l")) note = 14; // D
+
+        if (note >= 0)
+        { // if some key pressed...
+            Debug.Log((note + transpose));
+            //audio.pitch = Mathf.Pow(2, (float)((note + transpose) / 12.0));
+            audio.Play();
+        }
+    }
+}
---
+++ Labo3/Assets/Scripts/PartitionScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartitionScript : MonoBehaviour {
+
+    public GameObject Tooltip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var position = Input.mousePosition;
+        var partition = GameObject.Find("Partition").GetComponent<RectTransform>().rect;
+        partition.position = GameObject.Find("Partition").GetComponent<RectTransform>().position;
+
+        var partition1 = GameObject.Find("Partition1").GetComponent<RectTransform>().rect;
+        partition1.position = GameObject.Find("Partition1").GetComponent<RectTransform>().position;
+
+        var nbLine = 21;
+        var step = -20;
+        string[] notes = { "Si - B", "La - A", "Sol - G", "Fa - F", "Mi - E", "Ré - D", "Do - C" };
+
+        Debug.Log("mouse : " + position.ToString());
+        if (partition.Contains(Input.mousePosition) || partition1.Contains(Input.mousePosition))
+        {
+            Tooltip.SetActive(true);
+            Debug.Log("TESTESTEST");
+            Debug.Log(partition.position.ToString());
+            for (int i = 0; i < nbLine; i++)
+            {
+                if (between(position, (int)partition.position.y + i*step, (int)partition.position.y + (i+1)*step))
+                {
+                    Tooltip.GetComponent<Text>().text = notes[i%7];
+                    Tooltip.GetComponent<Transform>().position = new Vector3(position.x + 50, position.y + 50, 0);
+                }
+            }
+        }
+        else {
+            Tooltip.SetActive(false);
+        }
+	}
+
+    bool between (Vector3 position, int y1, int y2)
+    {
+        return (position.y > y1) && (position.y < y2);
+    }
+}
